b374a33049a83334db0195a15c7b2f8e1a245a99
Dani Malik <112042251+DaniMalik1@users.noreply.github.com>
2024-11-06 07:45:28 +0500

    Update Dani.py

--- Dani.py
+++ Dani.py
@@ -1,1 +1,15 @@
-import Dani
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from ex import menu
+    menu()
+elif bit == '32bit':
+    from ex import menu
+    menu()
+ 
